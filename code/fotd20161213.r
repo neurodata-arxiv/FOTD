c72c0bf13455032dfcbfb98cca9bf0e0cda027c4
gkiar <gkiar07@gmail.com>
2016-12-13 12:24:08 -0500

    added template

--- code/fotd20161213.r
+++
@@ -1,7 +0,0 @@
-
-require(meda)
-outf <- '~/Desktop/Ex10R55_resulVol1_MEDA.html'
-
-dat1 <- fread('~/neurodata/synaptome-stats/Code/Notebooks/Ex10R55_Synapsin1_2_vecs.csv')
-
-genHTML(dat1, outf, nmethod='cur', dmethod = 'cur')
